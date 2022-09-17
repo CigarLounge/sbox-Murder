c353ee4d3e4ccf6b241454fe94b6cb056857d7bc
Kole <novoselkosta@gmail.com>
2022-09-17 14:28:13 +0200

    Only pick up revolver if you're a bystander

--- code/Items/Weapons/Revolver.cs
+++ code/Items/Weapons/Revolver.cs
@@ -1,3 +1,4 @@
+using Sandbox;
 using SandboxEditor;
 
 namespace Murder;
@@ -7,4 +8,21 @@ namespace Murder;
 [EditorModel( "models/weapons/w_mr96.vmdl" )]
 [HammerEntity]
 [Title( "Revolver" )]
-public class Revolver : Weapon { }
+public class Revolver : Weapon
+{
+	public override float Damage => 200;
+	public override string ViewModelPath { get; } = "models/weapons/v_mr96.vmdl";
+	public override string WorldModelPath { get; } = "models/weapons/w_mr96.vmdl";
+
+	public override void SimulateAnimator( PawnAnimator animator )
+	{
+		base.SimulateAnimator( animator );
+
+		animator.SetAnimParameter( "holdtype", 1 );
+	}
+
+	public override bool CanCarry( Player carrier )
+	{
+		return carrier.Role == Role.Bystander && base.CanCarry( carrier );
+	}
+}
