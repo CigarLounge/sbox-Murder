ea2330c98bf52b922fc7385edd647f28797e9b93
Kole <novoselkosta@gmail.com>
2022-09-17 03:29:36 +0200

    Remove karma shit

--- code/Player/Player.Damage.cs
+++ code/Player/Player.Damage.cs
@@ -37,14 +37,6 @@ public partial class Player
 	[Net]
 	public TimeSince TimeSinceDeath { get; private set; }
 
-	public float DistanceToAttacker { get; set; }
-
-	/// <summary>
-	/// It's always better to use this than <see cref="Entity.LastAttackerWeapon"/>
-	/// because the weapon may be invalid.
-	/// </summary>
-	public CarriableInfo LastAttackerWeaponInfo { get; private set; }
-
 	public DamageInfo LastDamage { get; private set; }
 
 	public new float Health
@@ -52,45 +44,6 @@ public partial class Player
 		get => base.Health;
 		set => base.Health = Math.Clamp( value, 0, MaxHealth );
 	}
-
-	public new Entity LastAttacker
-	{
-		get => base.LastAttacker;
-		set
-		{
-			// If anyone uses a prop to kill someone.
-			if ( value is Prop prop && prop.LastAttacker is Player propAttacker )
-				base.LastAttacker = propAttacker;
-			else
-				base.LastAttacker = value;
-		}
-	}
-
-	/// <summary>
-	/// Whether or not they were killed by another Player.
-	/// This includes if the Player used a prop to kill them.
-	/// </summary>
-	public bool KilledByPlayer => LastAttacker is Player && LastAttacker != this;
-
-	/// <summary>
-	/// The damage factor scales how much damage the player deals, so if it is 0.9
-	/// then the player only deals 90% of his original damage.
-	/// </summary>
-	public float DamageFactor { get; set; } = 1f;
-
-	/// <summary>
-	/// If a player damages another team member that is "clean" (no active timer),
-	/// they'll end up with time being tacked onto this timer. A player will receive a
-	/// karma bonus for remaining "clean" (having no active timer) at the end of the round.
-	/// </summary>
-	public TimeUntil TimeUntilClean { get; set; } = 0f;
-
-	/// <summary>
-	/// The active karma starts equal to the base karma, but is updated as the
-	/// player damages/kills others. When a player damages/kills another, the
-	/// active karma is used to determine his karma penalty.
-	/// </summary>
-	public float ActiveKarma { get; set; }
 
 	private static readonly ColorGroup[] _healthGroupList = new ColorGroup[]
 	{
@@ -118,8 +71,7 @@ public partial class Player
 		TimeSinceDeath = 0;
 		Client.AddInt( "deaths" );
 
-		if ( KilledByPlayer )
-			LastAttacker.Client.AddInt( "kills" ); // TODO: Do we even want to keep track of this?
+		LastAttacker?.Client?.AddInt( "kills" ); // TODO: Do we even want to keep track of this?
 
 		Corpse = new Corpse( this );
 		RemoveAllDecals();
@@ -129,9 +81,7 @@ public partial class Player
 		EnableDrawing = false;
 		EnableTouch = false;
 
-		Inventory.DropAll();
 		DeleteFlashlight();
-		DeleteItems();
 
 		Event.Run( GameEvent.Player.Killed, this );
 		Game.Current.State.OnPlayerKilled( this );
@@ -161,9 +111,6 @@ public partial class Player
 		{
 			if ( Game.Current.State is not InProgress and not PostRound )
 				return;
-
-			if ( !info.Flags.HasFlag( DamageFlags.Slash ) )
-				info.Damage *= attacker.DamageFactor;
 		}
 
 		if ( info.Flags.HasFlag( DamageFlags.Bullet ) )
@@ -176,36 +123,15 @@ public partial class Player
 
 		LastAttacker = info.Attacker;
 		LastAttackerWeapon = info.Weapon;
-		LastAttackerWeaponInfo = (info.Weapon as Carriable)?.Info;
 		LastDamage = info;
 
 		Health -= info.Damage;
 		Event.Run( GameEvent.Player.TookDamage, this );
 
-		SendDamageInfo( To.Single( this ) );
-
 		this.ProceduralHitReaction( info );
 
 		if ( Health <= 0f )
 			OnKilled();
-	}
-
-	public void SendDamageInfo( To to )
-	{
-		Host.AssertServer();
-
-		SendDamageInfo
-		(
-			to,
-			LastAttacker,
-			LastAttackerWeapon,
-			LastAttackerWeaponInfo,
-			LastDamage.Damage,
-			LastDamage.Flags,
-			LastDamage.HitboxIndex,
-			LastDamage.Position,
-			DistanceToAttacker
-		);
 	}
 
 	private float GetBulletDamageMultipliers( ref DamageInfo info )
@@ -215,11 +141,7 @@ public partial class Player
 		var hitboxGroup = (HitboxGroup)GetHitboxGroup( info.HitboxIndex );
 
 		if ( hitboxGroup == HitboxGroup.Head )
-		{
-			var weaponInfo = GameResource.GetInfo<WeaponInfo>( info.Weapon.ClassName );
-
-			damageMultiplier *= weaponInfo?.HeadshotMultiplier ?? 2f;
-		}
+			damageMultiplier *= 2f;
 		else if ( hitboxGroup >= HitboxGroup.LeftArm && hitboxGroup <= HitboxGroup.Gear )
 			damageMultiplier *= 0.55f;
 
@@ -228,10 +150,8 @@ public partial class Player
 
 	private void ResetDamageData()
 	{
-		DistanceToAttacker = 0;
 		LastAttacker = null;
 		LastAttackerWeapon = null;
-		LastAttackerWeaponInfo = null;
 		LastDamage = default;
 	}
 
@@ -239,26 +159,6 @@ public partial class Player
 	public static void Deafen( float strength )
 	{
 		Audio.SetEffect( "flashbang", strength, velocity: 20.0f, fadeOut: 4.0f * strength );
-	}
-
-	[ClientRpc]
-	private void SendDamageInfo( Entity a, Entity w, CarriableInfo wI, float d, DamageFlags dF, int hI, Vector3 p, float dTA )
-	{
-		var info = DamageInfo.Generic( d )
-			.WithAttacker( a )
-			.WithWeapon( w )
-			.WithFlag( dF )
-			.WithHitbox( hI )
-			.WithPosition( p );
-
-		DistanceToAttacker = dTA;
-		LastAttacker = a;
-		LastAttackerWeapon = w;
-		LastAttackerWeaponInfo = wI;
-		LastDamage = info;
-
-		if ( IsLocalPawn )
-			Event.Run( GameEvent.Player.TookDamage, this );
 	}
 
 	[ClientRpc]
