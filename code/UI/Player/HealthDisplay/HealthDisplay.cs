d8a5620ac2eafe70137ac7f2e9f3ee6376cd4a76
Matthew Zegar <matthewzegar@gmail.com>
2022-09-19 16:29:16 -0700

    Give name an animation

--- code/UI/Player/HealthDisplay/HealthDisplay.cs
+++ code/UI/Player/HealthDisplay/HealthDisplay.cs
@@ -6,16 +6,35 @@ namespace Murder.UI;
 [UseTemplate]
 public class HealthDisplay : Panel
 {
-	public Label Name { get; init; }
-	public Label CluesCollected { get; init; }
+	private NameDisplay NameDisplay { get; set; }
+	private Label CluesCollected { get; init; }
 
-	public float BorderWidth { get; set; } = 5;
-	public float EdgeGap { get; set; } = 2;
-	public int Points { get; set; } = 64;
-	public float FillStart { get; set; } = .5f;
-	public float FillAmount { get; set; } = 0.37f;
-	public Color TrackColor { get; set; } = Color.White;
-	public Color FillColor { get; set; } = Color.Blue;
+	public override void Tick()
+	{
+		if ( Local.Pawn is not Player player )
+			return;
+
+		if ( player.IsSpectatingPlayer || player.IsAlive() )
+		{
+			NameDisplay ??= AddChild<NameDisplay>();
+		}
+		else
+		{
+			NameDisplay?.Delete();
+			NameDisplay = null;
+		}
+
+		CluesCollected.Text = $"{player.CluesCollected}";
+	}
+
+	// TODO: Wait for this fix https://github.com/Facepunch/sbox-issues/issues/2335
+	private float BorderWidth { get; set; } = 5;
+	private float EdgeGap { get; set; } = 2;
+	private int Points { get; set; } = 64;
+	private float FillStart { get; set; } = .5f;
+	private float FillAmount { get; set; } = 0.37f;
+	private Color TrackColor { get; set; } = Color.White;
+	private Color FillColor { get; set; } = Color.Blue;
 
 	public override void DrawBackground( ref RenderState state )
 	{
@@ -30,16 +49,5 @@ public class HealthDisplay : Panel
 
 		draw.Color = FillColor;
 		draw.CircleEx( center, radius - EdgeGap, radius - BorderWidth + EdgeGap, Points, FillStart * 360, (FillStart + FillAmount) * 360 );
-	}
-
-	public override void Tick()
-	{
-		if ( Local.Pawn is not Player player )
-			return;
-
-		Name.Text = player.AssignedName;
-		Name.Style.FontColor = player.AssignedColour;
-
-		CluesCollected.Text = $"{player.CluesCollected}";
 	}
 }
