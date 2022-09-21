514e2efde8947750d984b11a384fb6e95c85b8ca
Matthew Zegar <matthewzegar@gmail.com>
2022-09-20 20:33:26 -0700

    Inactive carriable has lower opacity

--- code/UI/Player/InventoryWrapper/InventoryWrapper.cs
+++ code/UI/Player/InventoryWrapper/InventoryWrapper.cs
@@ -6,6 +6,9 @@ namespace Murder.UI;
 [UseTemplate]
 public class InventoryWrapper : Panel
 {
+	private readonly float _activeOpacity = 1f;
+	private readonly float _inactiveOpacity = 0.4f;
+
 	private Slot _holsterSlot;
 	private Slot _carriableSlot;
 
@@ -14,17 +17,25 @@ public class InventoryWrapper : Panel
 		if ( Local.Pawn is not Player player )
 			return;
 
-		var hasCarriableSlot = player.CurrentPlayer.IsAlive() && player.CurrentPlayer.Carriable.IsValid();
-		if ( hasCarriableSlot )
-			_carriableSlot ??= AddSlot( new Slot( player.CurrentPlayer.Carriable.Name, InputButton.Slot2 ) );
-		else
-			DeleteSlot( ref _carriableSlot );
-
 		var hasHolsterSlot = player.CurrentPlayer.IsAlive();
 		if ( hasHolsterSlot )
 			_holsterSlot ??= AddSlot( new Slot( "Holster", InputButton.Slot1 ) );
 		else
 			DeleteSlot( ref _holsterSlot );
+
+		var hasCarriableSlot = hasHolsterSlot && player.CurrentPlayer.Carriable.IsValid();
+		if ( hasCarriableSlot )
+			_carriableSlot ??= AddSlot( new Slot( player.CurrentPlayer.Carriable.Name, InputButton.Slot2 ) );
+		else
+			DeleteSlot( ref _carriableSlot );
+
+		var isCarriableSlotActive = player.CurrentPlayer.ActiveCarriable.IsValid();
+
+		if ( _holsterSlot.IsValid() )
+			_holsterSlot.Style.Opacity = isCarriableSlotActive ? _inactiveOpacity : _activeOpacity;
+
+		if ( _carriableSlot.IsValid() )
+			_carriableSlot.Style.Opacity = isCarriableSlotActive ? _activeOpacity : _inactiveOpacity;
 	}
 
 	private Slot AddSlot( Slot slot )
