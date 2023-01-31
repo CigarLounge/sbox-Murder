2250906d924635289de806da83922bf2d9329b6e
Matthew Zegar <matthewzegar@gmail.com>
2023-01-30 21:10:38 -0800

    Make sure scoreboard gets updated

--- code/Util/Utils.cs
+++ code/Util/Utils.cs
@@ -1,7 +1,6 @@
 using Sandbox;
 using System;
 using System.Collections.Generic;
-using System.Text.Json;
 
 namespace Murder;
 
@@ -10,12 +9,9 @@ public static class Utils
 	public static List<Player> GetPlayersWhere( Func<Player, bool> predicate )
 	{
 		List<Player> players = new();
-
 		foreach ( var client in Game.Clients )
-		{
 			if ( client.Pawn is Player player && predicate.Invoke( player ) )
 				players.Add( player );
-		}
 
 		return players;
 	}
@@ -23,23 +19,10 @@ public static class Utils
 	public static List<IClient> GetClientsWhere( Func<Player, bool> predicate )
 	{
 		List<IClient> clients = new();
-
 		foreach ( var client in Game.Clients )
-		{
 			if ( client.Pawn is Player player && predicate.Invoke( player ) )
 				clients.Add( client );
-		}
 
 		return clients;
 	}
-
-	public static async void DelayAction( float seconds, Action callback )
-	{
-		await GameTask.DelaySeconds( seconds );
-
-		callback?.Invoke();
-	}
-
-	public static byte[] Serialize<T>( this T data ) => JsonSerializer.SerializeToUtf8Bytes( data );
-	public static T Deserialize<T>( this byte[] bytes ) => JsonSerializer.Deserialize<T>( bytes );
 }
